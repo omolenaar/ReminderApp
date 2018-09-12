3a9d2fc4412d32636874770789e2e7978739db5a
Olga <olga.molenaar@gmail.com>
2018-09-12 11:21:09 +0200

    Some details not in place; working list below the titlebar.

--- app/src/main/java/com/example/android/reminderdemo/MainActivity.java
+++ app/src/main/java/com/example/android/reminderdemo/MainActivity.java
@@ -5,11 +5,26 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    //fields
+    private List<Reminder> mReminders;
+    private ArrayAdapter mAdapter;
+    private ListView mListView;
+    private EditText mNewReminderText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +33,33 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //initialize
+        mListView = findViewById(R.id.listView_main);
+        mNewReminderText = findViewById(R.id.editText_main);
+        mReminders = new ArrayList<>();
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mReminders);
+        mListView.setAdapter(mAdapter);
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                String text = mNewReminderText.getText().toString();
+                Reminder newReminder = new Reminder(text);
+                //Check if text was entered
+                if (!(TextUtils.isEmpty(text))) {
+                    mReminders.add(newReminder);
+                    //refresh
+                    mAdapter.notifyDataSetChanged();
+                    //clear contents
+                    mNewReminderText.setText("");
+                } else {
+                    Snackbar.make(view, "Add reminder", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+
             }
         });
     }
